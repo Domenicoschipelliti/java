bb81b2bf489baa16f9677ae190b36096f0c853d2
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2023-11-27 14:47:45 +0100

    Add files via upload

---
+++ JAVA/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static  void  main(String[] args){
+
+        int number=2;
+        int number2=4;
+
+        System.out.println(number*number2);
+
+        String hello="ciao ho ";
+        int number3=5;
+
+        System.out.println(hello + number3 + " anni" );
+
+
+
+    }
+
+
+
+}
